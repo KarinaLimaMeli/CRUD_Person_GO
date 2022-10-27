4dd18e62dc16b2630ee5210ffa178cbac31ee1c5
KarinaLimaMeli <karina.klima@mercadolivre.com>
2022-10-27 19:10:44 -0300

    crud

--- domain/entities.go
+++ domain/entities.go
@@ -1,14 +1,11 @@
 package domain
 
 type Person struct {
-	ID int `json:"id"`
-	Name string `json: "name"`
-	Age int `json: "age"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 type People struct {
-	People []Person `json: "people"`
-
-
-
+	People []Person `json:"people"`
 }
--- main.go
+++ main.go
@@ -4,38 +4,117 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/KarinaLimaMeli/crud-api/domain"
+	"github.com/KarinaLimaMeli/crud-api/person"
 )
-
 func main() {
+	personService, err := person.NewService("person.json")
+	if err != nil {
+		fmt.Printf("Error trying to creating personService: %s\n", err.Error())
+	}
 
-
-	http.HandleFunc("/person/", func(w http.ResponseWriter,r *http.Request){
-
+	http.HandleFunc("/person/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			path := strings.TrimPrefix(r.URL.Path, "/person/")
+			if path == "" {
+				// list all people
+				w.WriteHeader(http.StatusOK)
+				w.Header().Set("Content-Type", "application/json")
+				err = json.NewEncoder(w).Encode(personService.List())
+				if err != nil {
+					http.Error(w, "Error trying to list people", http.StatusInternalServerError)
+					return
+				}
+			} else {
+				personID, err := strconv.Atoi(path)
+				if err != nil {
+					http.Error(w, "Invalid id given. person ID must be an integer", http.StatusBadRequest)
+					return
+				}
+				person, err := personService.GetByID(personID)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusNotFound)
+					return
+				}
+				w.WriteHeader(http.StatusOK)
+				w.Header().Set("Content-Type", "application/json")
+				err = json.NewEncoder(w).Encode(person)
+				if err != nil {
+					http.Error(w, "Error trying to get person", http.StatusInternalServerError)
+					return
+				}
+			}
+			return
+		}
 		if r.Method == "POST" {
-			
 			var person domain.Person
 			err := json.NewDecoder(r.Body).Decode(&person)
 			if err != nil {
-				fmt.Printf("Error trying to decode body. Body should be a json. Error: %s", err.Error())
+				fmt.Printf("Error trying to decode body. Body should be a json. Error: %s\n", err.Error())
 				http.Error(w, "Error trying to create person", http.StatusBadRequest)
 				return
 			}
-			if person.ID <= 0{
-				http.Error(w, "Error trying to create person. ID should be a positive integer", http.StatusBadRequest)
+			if person.ID <= 0 {
+				http.Error(w, "person ID should be a positive integer", http.StatusBadRequest)
 				return
 			}
-			// criar pessoa
+
 			err = personService.Create(person)
 			if err != nil {
-				fmt.Printf("Error trying to create person: %s", err.Error())
+				fmt.Printf("Error trying to create person: %s\n", err.Error())
 				http.Error(w, "Error trying to create person", http.StatusInternalServerError)
 				return
 			}
 			w.WriteHeader(http.StatusCreated)
+			return
 		}
-		http.Error(w, "Not Implemented", http.StatusInternalServerError)
-})
+		if r.Method == "DELETE" {
+			path := strings.TrimPrefix(r.URL.Path, "/person/")
+			if path == "" {
+				http.Error(w, "ID is required to delete a person", http.StatusBadRequest)
+				return
+			} else {
+				personID, err := strconv.Atoi(path)
+				if err != nil {
+					http.Error(w, "Invalid id given. person ID must be an integer", http.StatusBadRequest)
+					return
+				}
+				err = personService.DeleteByID(personID)
+				if err != nil {
+					fmt.Printf("Error trying to delete person: %s\n", err.Error())
+					http.Error(w, "Error trying to delete person", http.StatusInternalServerError)
+					return
+				}
+				w.WriteHeader(http.StatusOK)
+			}
+			return
+		}
+		if r.Method == "PUT" {
+			var person domain.Person
+			err := json.NewDecoder(r.Body).Decode(&person)
+			if err != nil {
+				fmt.Printf("Error trying to decode body. Body should be a json. Error: %s\n", err.Error())
+				http.Error(w, "Error trying to update person", http.StatusBadRequest)
+				return
+			}
+			if person.ID <= 0 {
+				http.Error(w, "person ID should be a positive integer", http.StatusBadRequest)
+				return
+			}
+
+			err = personService.Update(person)
+			if err != nil {
+				fmt.Printf("Error trying to update person: %s\n", err.Error())
+				http.Error(w, "Error trying to update person", http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
